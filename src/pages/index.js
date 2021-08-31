f47358c7b730b3834a98f8d0a3abc65e8c253082
JongwonYoun <jeffyoun@naver.com>
2021-08-31 21:24:27 +0900

    jeniffer 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,6 +24,15 @@ export default ({ data }) => {
         <Layout>
             <Helmet>
                 <meta name="naver-site-verification" content="866db3f93b637805d8f80dcdf45c289f739b2da0" />
+                <script>{`   
+                    (function(j,en,ni,fer) {
+                    j['dmndata']=[];j['jenniferFront']=function(args){window.dmndata.push(args)};
+                    j['dmnaid']=fer;j['dmnatime']=new Date();j['dmnanocookie']=false;j['dmnajennifer']='JENNIFER_FRONT@INTG';
+                    var b=Math.floor(new Date().getTime() / 60000) * 60000;var a=en.createElement(ni);
+                    a.src='https://d-collect.jennifersoft.com/'+fer+'/demian.js?'+b;a.async=true;
+                    en.getElementsByTagName(ni)[0].parentNode.appendChild(a);
+                }(window,document,'script','402db92c'));
+                `}</script>
             </Helmet>
             <SEO title="Home"/>
             <Bio author={author} language={language}/>
